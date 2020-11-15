e4a792a1338b15a843ac8bf7c0b8f46eb2053740
Andrea Wu <lixianhyun@gmail.com>
2020-11-14 22:50:11 -0800

    add click evenListener to 'canvas' element

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 window.onload = function() {
     const canvas = document.getElementById("canvas");
-    console.log(canvas);
+
+    canvas.addEventListener("click", function(event){
+        let x = event.clientX;
+        let y = event.clientY;
+
+        console.log("x: " + x + "; y: " + y);
+    })
 }
