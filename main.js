026bde3f2b933565abe18ea8af9178c59c7b3eac
Andrea Wu <lixianhyun@gmail.com>
2020-11-15 20:54:25 -0800

    setInterval to 'canvas' element

--- main.js
+++ main.js
@@ -4,24 +4,47 @@ window.onload = function() {
     canvas.height = window.innerHeight;
 
     const c = canvas.getContext("2d"); // **--> c stand for 'context' in canvas
+    let x;
+    let y;
+    let timer;
 
-    canvas.addEventListener("mousemove", function(event){
-        let x = event.clientX;
-        let y = event.clientY;
+    canvas.onmousedown = draw;
+    canvas.onmousemove = getCoordinates;
+    canvas.onmouseout = clearIntv;
+    canvas.onmouseup = clearIntv;
 
-        c.fillRect(x, y, 20, 20); // **--> c.fillReact(x, y, width, height);
-        let gradient = c.createLinearGradient(0, 0, canvas.width, canvas.height);
-        gradient.addColorStop(0, 'red');
-        gradient.addColorStop(0.1, 'orange');
-        gradient.addColorStop(0.2, 'yellow');
-        gradient.addColorStop(0.3, 'lightgreen');
-        gradient.addColorStop(0.4, '#8ED6FF');
-        gradient.addColorStop(0.5, '#004CB3');
-        gradient.addColorStop(0.6, 'purple');
-        gradient.addColorStop(0.7, 'pink');
-        gradient.addColorStop(0.8, 'lightpink');
-        gradient.addColorStop(0.9, 'coral');
-        gradient.addColorStop(1, 'wheat');   
-        c.fillStyle = gradient;
-    })
+    function getCoordinates(event){
+        x = event.clientX;
+        y = event.clientY;
+    }
+
+    function draw(event) {
+        timer = setInterval (() => canvasColor(event), 1);  
+        console.log(timer);
+    };
+
+    function canvasColor() {
+
+            c.beginPath();
+            c.fillRect(x, y, 20, 20); // **--> c.fillReact(x, y, width, height);
+            let gradient = c.createLinearGradient(0, 0, canvas.width, canvas.height);
+            gradient.addColorStop(0, 'red');
+            gradient.addColorStop(0.1, 'orange');
+            gradient.addColorStop(0.2, 'yellow');
+            gradient.addColorStop(0.3, 'lightgreen');
+            gradient.addColorStop(0.4, '#8ED6FF');
+            gradient.addColorStop(0.5, '#004CB3');
+            gradient.addColorStop(0.6, 'purple');
+            gradient.addColorStop(0.7, 'pink');
+            gradient.addColorStop(0.8, 'lightpink');
+            gradient.addColorStop(0.9, 'coral');
+            gradient.addColorStop(1, 'wheat');   
+            c.fillStyle  = gradient;
+            c.stroke();
+
+    }
+
+    function clearIntv(){
+        clearInterval(timer);
+    }
 }
